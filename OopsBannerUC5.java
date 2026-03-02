9748632e7ad1b43714e67bb8372337689ab371dc
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-02 09:06:18 +0530

    Added UC5.java

---
+++ OopsBannerUC5.java
@@ -0,0 +1,21 @@
+public class OopsBannerUC5 {
+
+    public static void main(String[] args) {
+
+        // UC5: Inline Array Initialization using String.join()
+        String[] banner = {
+                String.join("", " *****   ", " *****   ", " *****   ", " *****   "),
+                String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *  "),
+                String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *  "),
+                String.join("", "*     *  ", " *****   ", "*     *  ", " *****   "),
+                String.join("", "*     *  ", "*        ", "*     *  ", "*        "),
+                String.join("", "*     *  ", "*        ", "*     *  ", "*        "),
+                String.join("", " *****   ", "*        ", " *****   ", "*        ")
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
